a48f79c97ab0ff2ba87c5201cfb6611338279d57
Ilhammursidi <imursidi37@gmail.com>
2026-02-26 12:42:02 +0700

    Update minitask5.js

--- minitask5.js
+++ minitask5.js
@@ -1,15 +1,11 @@
 const account = {
   id: 120,
   username: "ilham",
-  password: "ilhamlama",
-};
-
-const editPassword = {
-  ...account,
-  password: "baru",
+  password: "ilham123"
 };
 
 function editPass() {
+  let editPassword = { ...account, password: "baru" };
   if (editPassword.password.length < 5) {
     console.log(account);
     console.log("minimum password adalah 5 karakter");
